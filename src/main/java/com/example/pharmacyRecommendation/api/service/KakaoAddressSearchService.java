b2c57c07df42fd7eb4e81f2cc8054509cfba23ee
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-06-23 18:02:03 +0900

    kakao 주소검색 api 구현하기

---
+++ src/main/java/com/example/pharmacyRecommendation/api/service/KakaoAddressSearchService.java
@@ -0,0 +1,40 @@
+package com.example.pharmacyRecommendation.api.service;
+
+import com.example.pharmacyRecommendation.api.dto.KakaoApiResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class KakaoAddressSearchService {
+
+    private final RestTemplate restTemplate;
+    private final KakaoUrlBuilderService kakaoUrlBuilderService;
+
+    @Value("${kakao.rest.api.key}")
+    private String kakaoRestApiKey;
+
+    public KakaoApiResponseDto requestAddressSearch(String address) {
+
+        if (ObjectUtils.isEmpty(address)) return null;
+
+        URI uri = kakaoUrlBuilderService.builderByAddressSearch(address);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set(HttpHeaders.AUTHORIZATION, "KakaoAK " + httpHeaders);
+        HttpEntity httpEntity = new HttpEntity<>(httpHeaders);
+
+        // kakao api 호출
+        return restTemplate.exchange(uri, HttpMethod.GET, httpEntity, KakaoApiResponseDto.class).getBody();
+    }
+}
