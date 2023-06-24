cee28fb23b88e1022a665f810f701c6920b87425
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-06-24 17:22:54 +0900

    JPA Dirty Checking

---
+++ src/test/groovy/com/example/pharmacyRecommendation/pharmacy/service/PharmacyRepositoryServiceTest.groovy
@@ -0,0 +1,68 @@
+package com.example.pharmacyRecommendation.pharmacy.service
+
+import com.example.pharmacyRecommendation.AbstractIntegrationContainerBaseTest
+import com.example.pharmacyRecommendation.pharmacy.entity.Pharmacy
+import com.example.pharmacyRecommendation.pharmacy.repository.PharmacyRepository
+import org.springframework.beans.factory.annotation.Autowired
+
+class PharmacyRepositoryServiceTest extends AbstractIntegrationContainerBaseTest {
+
+    @Autowired
+    private PharmacyRepositoryService pharmacyRepositoryService
+
+    @Autowired
+    private PharmacyRepository pharmacyRepository
+
+    def setup() {
+        pharmacyRepository.deleteAll()
+    }
+
+    def "PharmacyRepository update - dirty checking success"() {
+        given:
+        String address = "서울 특별시 성북구 종암동"
+        String modifiedAddress = "서울 광진구 구의동"
+
+        String name = "은혜 약국"
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .build()
+
+        when:
+        def entity = pharmacyRepository.save(pharmacy)
+
+        then:
+        pharmacyRepositoryService.updateAddress(entity.getId(), modifiedAddress)
+
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.get(0).getPharmacyAddress() == modifiedAddress
+    }
+
+    def "PharmacyRepository update - dirty checking fain"() {
+        given:
+        String address = "서울 특별시 성북구 종암동"
+        String modifiedAddress = "서울 광진구 구의동"
+
+        String name = "은혜 약국"
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .build()
+
+        when:
+        def entity = pharmacyRepository.save(pharmacy)
+
+        then:
+        pharmacyRepositoryService.updateAddressWithoutTransaction(entity.getId(), modifiedAddress)
+
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.get(0).getPharmacyAddress() == address
+    }
+    
+}
