608bf4a8e1ebc7187eb1393a7f9c65cee53d0480
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-06-24 16:56:32 +0900

    Kakao 주소검색 api 테스트 작성

--- src/test/groovy/com/example/pharmacyRecommendation/pharmacy/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/com/example/pharmacyRecommendation/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -2,13 +2,17 @@ package com.example.pharmacyRecommendation.pharmacy.repository
 
 import com.example.pharmacyRecommendation.AbstractIntegrationContainerBaseTest
 import com.example.pharmacyRecommendation.pharmacy.entity.Pharmacy
-import com.example.pharmacyRecommendation.pharmacy.repository.PharmacyRepository
 import org.springframework.beans.factory.annotation.Autowired
 
 class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
 
     @Autowired
     private PharmacyRepository pharmacyRepository
+
+    def setup() {
+        pharmacyRepository.deleteAll()
+    }
+
 
     def "PharmacyRepository save"() {
         given:
@@ -32,5 +36,28 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
         result.getPharmacyName() == name
         result.getLatitude() == latitude
         result.getLongitude() == longitude
+    }
+
+
+    def "PharmacyRepository saveAll"() {
+        given:
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        pharmacyRepository.saveAll(Arrays.asList(pharmacy))
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.size() == 1
     }
 }
